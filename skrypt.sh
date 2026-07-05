af477dc472d34eb9bcc3b5e1c988975f2163157e
5h4d0v <j.kolanek123@gmail.com>
2026-07-05 22:47:38 +0200

    Dodano tworzenie 100 plikow log

--- skrypt.sh
+++ skrypt.sh
@@ -1,10 +1,27 @@
 #!/usr/bin/env bash
 
+SCRIPT_NAME="$(basename "$0")"
+
 case "$1" in
     --date)
         date
         ;;
+
+    --logs)
+        for i in $(seq 1 100); do
+            filename="log${i}.txt"
+
+            echo "Nazwa pliku: $filename" > "$filename"
+            echo "Utworzony przez skrypt: $SCRIPT_NAME" >> "$filename"
+            echo "Data utworzenia: $(date)" >> "$filename"
+        done
+
+        echo "Utworzono 100 plikow log."
+        ;;
+
     *)
-        echo "Uzyj: ./skrypt.sh --date"
+        echo "Uzyj:"
+        echo "./skrypt.sh --date"
+        echo "./skrypt.sh --logs"
         ;;
 esac
